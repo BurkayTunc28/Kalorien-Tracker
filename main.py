a8935e706d2b8d19ee25fc4d3f851fa47eccb4fc
Burkay <burkaytunc29@gmail.com>
2026-02-03 18:25:49 +0100

    UV Dependency Management für ATL-Projekt

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ name = "Burkay"
 
 print("hallo " + name)
 
+
+#Hier sind die Details zur Berechnung des Kalorienbedarfs pro Tag:
+# Grundumsatz (BMR)
+#-Formel nach Harris-Benedict:
+# Männer: 66,47+(13,7 x Gewicht in kg) + (5 x Grösse in cm) - (6,8 x Alter in Jahren)
+# Frauen: 655,1+(9,6 x Gewicht in kg) + (1,8 x Grösse in cm) - (4,7 x Alter in Jahren)
+# 
+# Gesamtumsatz (Tagesbedarf):
+# Grundumsatz \(\times \) PAL-Faktor (Physical Activity Level).
+# 
+# Beispiele für PAL-Faktoren:
+# 1,2 (nur sitzend/liegend), 1,4–1,5 (überwiegend sitzend, wenig Aktivität), 1,6–1,7 (sitzend, gehend), 1,8–1,9 (körperlich anstrengende Arbeit/Sport).
+
+
+
+
+
+
+
+
